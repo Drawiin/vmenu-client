74790bba2349ac5ac1b1d390306c940d7939dc54
vinicius santos <viniciussantos@jera.com.br>
2020-11-06 00:38:33 -0400

    bottom nav

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,37 @@
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import RestoreIcon from '@material-ui/icons/Restore'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import LocationOnIcon from '@material-ui/icons/LocationOn'
+import { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 0
+  }
+})
+
 const Home: React.FC = () => {
-  return <h1>Bem vindo ao seu menu digital</h1>
+  const classes = useStyles()
+  const [value, setValue] = useState(0)
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue)
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+    </BottomNavigation>
+  )
 }
 
 export default Home
