daf78b23181bdd74bf7d59c46e20755286a894ba
vinicius santos <viniciussantos@jera.com.br>
2020-11-17 22:41:27 -0400

    refactoring onboarding v2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box'
-import MenuBookIcon from '@material-ui/icons/MenuBook'
-import Qrcode from 'mdi-material-ui/Qrcode'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Theme, withStyles } from '@material-ui/core/styles'
@@ -13,8 +11,6 @@ const StyledButton = withStyles((theme: Theme) => ({
   },
   root: {
     padding: theme.spacing(2),
-    display: 'flex',
-    justifyContent: 'space-between',
     marginTop: theme.spacing(1)
   }
 }))(Button)
@@ -28,46 +24,44 @@ const SignIn: React.FC = () => {
       height="100vh"
       width={1.0}
       paddingX={2}
-      paddingBottom={2}
+      paddingBottom={5}
     >
       <Box display="flex" flexDirection="column" alignItems="center">
-        <Box width={1.0} overflow="hidden" marginTop={10} marginBottom={5}>
+        <Box width={1.0} overflow="hidden" marginTop={17} marginBottom={5}>
           <Image
-            src="/dinner.svg"
+            src="/people.svg"
             layout="responsive"
             width="100%"
             height="55%"
           />
         </Box>
 
-        <Typography variant="h5" color="textSecondary" align="center">
+        <Typography variant="h6" color="textSecondary" align="center">
           Bem Vindo Ao Seu Cardápio Virtual, Escaneie o QR Code da Sua Mesa Para
           Continuar.
         </Typography>
       </Box>
 
-      <Box display="flex" flexDirection="column">
+      <Box display="flex">
+        <Link href="/menu">
+          <StyledButton
+            style={{ marginRight: 16 }}
+            fullWidth
+            disableElevation
+            variant="outlined"
+            color="secondary"
+          >
+            Cardápio
+          </StyledButton>
+        </Link>
         <Link href="/qrcode">
           <StyledButton
             fullWidth
             disableElevation
             variant="contained"
             color="primary"
-            endIcon={<Qrcode />}
           >
             Ler QR code
-          </StyledButton>
-        </Link>
-
-        <Link href="/menu">
-          <StyledButton
-            fullWidth
-            disableElevation
-            variant="outlined"
-            color="secondary"
-            endIcon={<MenuBookIcon />}
-          >
-            Cardápio
           </StyledButton>
         </Link>
       </Box>
