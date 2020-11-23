f271532d8461dfb5d8a9b0ac98d721a31297db89
vinicius santos <viniciussantos@jera.com.br>
2020-11-23 18:01:58 -0400

    oii

--- src/pages/read-table.tsx
+++ src/pages/read-table.tsx
@@ -1,10 +1,19 @@
-import QrReader from 'react-qr-reader'
+import dynamic from 'next/dynamic'
 
 import Box from '@material-ui/core/Box'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+const QrReader = dynamic(() => import('react-qr-reader'), {
+  ssr: false,
+  loading: () => {
+    const Loading = <CircularProgress />
+    return Loading
+  }
+})
 
 const ReadTable: React.FC = () => {
-  const onScan = (result: string) => {
-    console.log(result)
+  const onScan = (result?: string) => {
+    result && console.log(result)
   }
 
   return (
