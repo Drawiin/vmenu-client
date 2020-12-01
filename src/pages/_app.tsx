c99460bb70e08f7ac874d0f5b39c48dda3d930bc
gustavo <gustavoferri13@gmail.com>
2020-11-30 21:12:07 -0400

    add isauthenticated component to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,19 @@ import Head from 'next/head'
 import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { AppProps } from 'next/dist/next-server/lib/router/router'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 
-import BottomNavigationLayout from '@presentation/components/layout/BottomNavigationLayout'
 import theme from '@presentation/styles/theme'
 import useOrderContext from '@domain/utils/hooks/useOrderContext'
+import useUserContext from '@domain/utils/hooks/useUserContext'
 import OrderContext from '@domain/utils/OrderContext'
+import UserContext from '@domain/utils/UserContext'
+import UserModal from '@presentation/components/User'
+import IsAuthenticated from '@presentation/utils/isAuthenticated'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps, router }) => {
   const value = useOrderContext()
+  const userContext = useUserContext()
 
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
@@ -28,18 +32,20 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps, router }) => {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-      <OrderContext.Provider value={value}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          {router.pathname.includes('/main') ? (
-            <BottomNavigationLayout>
-              <Component {...pageProps} />
-            </BottomNavigationLayout>
-          ) : (
-            <Component {...pageProps} />
-          )}
-        </ThemeProvider>
-      </OrderContext.Provider>
+      <UserContext.Provider value={userContext}>
+        <OrderContext.Provider value={value}>
+          <ThemeProvider theme={theme}>
+            <UserModal />
+            <CssBaseline />
+            <IsAuthenticated
+              user={userContext.user}
+              Component={Component}
+              pageProps={pageProps}
+              router={router}
+            />
+          </ThemeProvider>
+        </OrderContext.Provider>
+      </UserContext.Provider>
     </>
   )
 }
